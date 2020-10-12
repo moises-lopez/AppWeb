5eda3e6dee71a99a92c767b904cb4b26171632da
moises-lopez <moyses979@gmail.com>
2020-10-11 22:57:19 -0500

    ADDING POKEMON CRUD FRONT AND MONGO

--- POKEMON CRUD/cliente/utils.js
+++ POKEMON CRUD/cliente/utils.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", (_) => {
   // use here what you have in utils.js
   document.getElementById("add-item").addEventListener("click", getPokemon);
+  document.getElementById("getAll").addEventListener("click", getAll);
+  document.getElementById("getID").addEventListener("click", getID);
+  document.getElementById("delete").addEventListener("click", toDelete);
+  document.getElementById("update").addEventListener("click", handleUpdate);
+
 });
 
 let get_element_li = (
@@ -14,33 +19,132 @@ let get_element_li = (
 ) => {
   return `<div class="added-pokemon pokecard" ><h1>Name: ${name} id: ${id} </h1> <div><img src="${image}"></div> <div>types: ${types}<div>weight: ${weight} height: ${height} <div> base experience: ${base_experience} </div>`;
 };
+
+let get_element_li_not_pokemon = (
+  name,
+  type,
+  data
+) => {
+  return `<div class="added-pokemon pokecard"><h1>Name: ${name} Type Card: ${type} </h1> <div> Data: ${data} </div>`;
+};
 // are id, weight, all the types, height, base_experience
 
+function handleUpdate(){
+  let name = document.querySelector("#update-pokemon").value;
+  let data = document.querySelector("#data-update").value
+  axios
+  .put(`http://localhost:3000/put/${name}`, {
+    params: {
+    name : name,
+    data : data
+  }})
+    .then((response) => {
+      console.log(response)
+      //div.innerHTML = "";
+    })
+    .catch((err) => {
+      //div.innerHTML = "NO EXISTE";
+    });
+}
 function addPokemon(datos) {
+  let myData = datos.data
   let typeNames = [];
-  datos.types.forEach((typeData) => {
-    typeNames.push(typeData.type.name);
-  });
+  let arrayTypes = myData.typeNames
+  arrayTypes.forEach((typeData) => {
+     typeNames.push(typeData);
+   });
   let template = get_element_li(
-    datos.name,
-    datos.id,
-    datos.weight,
-    datos.height,
-    datos.base_experience,
-    datos.sprites.front_default,
+    myData.name,
+    myData.id,
+    myData.weight,
+    myData.height,
+    myData.base_experience,
+    myData.sprites.front_default,
     typeNames
   );
-
   document.getElementById("items").innerHTML += template;
+  //document.getElementById("items").innerHTML = datos.name
+}
+
+function addCard(datos){
+  console.log(datos)
+  let myData = datos
+  let template = get_element_li_not_pokemon(myData.name, myData.typecard, myData.data)
+  document.getElementById("items").innerHTML += template;
+}
+
+function toDelete(){
+  let name = document.querySelector("#delete-pokemon").value;
+  axios
+    .delete(`http://localhost:3000/delete/${name}`)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((err) => {
+      //div.innerHTML = "NO EXISTE";
+    });
+}
+
+function getAll(){
+  document.getElementById("items").innerHTML = ""
+  axios
+    .get(`http://localhost:3000/get`)
+    .then((response) => {
+      console.log("response", response)
+      pokearray = response.data
+      //console.log(pokearray)
+      pokearray.forEach(element => {
+        if(element.typecard == "pokemon"){
+          addPokemon(element)
+        }else{
+          addCard(element)
+        }
+      })
+      //div.innerHTML = "";
+    })
+    .catch((err) => {
+      //div.innerHTML = "NO EXISTE";
+    });
+}
+
+function getID(){
+  document.getElementById("items").innerHTML = ""
+  let name = document.querySelector("#get-pokemon-name").value;
+  axios
+    .get(`http://localhost:3000/get/${name}`)
+    .then((response) => {
+      console.log("response", response)
+      pokearray = response.data
+      //console.log(pokearray)
+      pokearray.forEach(element => {
+        if(element.typecard == "pokemon"){
+          addPokemon(element)
+        }else{
+          addCard(element)
+        }
+      })
+      //div.innerHTML = "";
+    })
+    .catch((err) => {
+      //div.innerHTML = "NO EXISTE";
+    });
 }
 
 function getPokemon() {
   let name = document.querySelector("#pokemon-name").value;
+  let type = document.querySelector("#type").value
+  let data = document.querySelector("#data").value
   let errorP = document.getElementById("error");
   axios
-    .get("http://localhost:3000/" + name)
+    .post(`http://localhost:3000/add`, {
+      params: {
+      type : type,
+      name : name,
+      data : data
+    }})
     .then((response) => {
-      addPokemon(response.data);
+      //refreshDeck();
+      console.log(response)
       errorP.innerHTML = "";
     })
     .catch((err) => {
--- POKEMON CRUD/server/server.js
+++ POKEMON CRUD/server/server.js
@@ -10,19 +10,84 @@ app.use(
   })
 );
 
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://123:123@cluster0.qcopr.mongodb.net/POKEMONGAME?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("POKEMONGAME");
+
+  app.post("/add", async (req, res) => {
+    let params = processParams(req);
+    params = params.params
+    console.log(params)
+    let typecard = params.type;
+    let name = params.name;
+    let data;
+    //console.log(params.params.name, typecard)
+    if (typecard != "pokemon") {
+      data = params.data;
+    } else {
+      data = await getPokemonCard(name);
+    }
+    let card = { name: name, typecard: typecard, data: data };
+    dbo.collection("DECK").insertOne(card);
+    res.send("Success");
+  });
+
+
+  app.get("/get/:id", (req, res) => {
+    let params = processParams(req);
+    let name = params.id; //Utilizamos el nombre de la carta como ID
+    dbo.collection("DECK").find({name : name}).toArray(function(err, result){ res.send(result) });
+    
+  });
+  
+  app.delete("/delete/:id", function (req, res) {
+    console.log("DELETE?")
+    let params = processParams(req);
+    let name = params.id; //Utilizamos el nombre de la carta como ID
+    dbo.collection("DECK").remove({name : name})
+    
+    res.send("Deleted")
+  });
+  
+  app.get("/get", function (req, res) {
+    dbo.collection("DECK").find().toArray(function(err, result){ res.send(result) });
+  });
+  
+  app.put("/put/:id", function (req, res) {
+    let params = processParams(req);
+    params = params.params
+    console.log(params)
+    let myname = params.name;
+    let mydata = params.data; //el parseo de la data será desde front, data contiene todo
+    var myquery = { name : myname };
+    var newvalues = { $set: {data: mydata } };
+    console.log(mydata, myname)
+    dbo.collection("DECK").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+    });
+    res.send("Success");
+  });
+  
+});
+
 function processParams(req) {
   return Object.assign({}, req.body, req.params, req.query);
 }
 
 let mazo = {};
 
-function getPokemon(name) {
+const getPokemon = (name) => {
   return axios
-    .get("https://pokeapi.co/api/v2/pokemon/" + name)
+    .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.data);
-}
+};
 
-async function getPokemonCard(name) {
+const getPokemonCard = async (name) => {
   let rawData = await getPokemon(name);
   let typeNames = [];
   rawData.types.forEach((typeData) => {
@@ -34,61 +99,13 @@ async function getPokemonCard(name) {
     weight: rawData.weight,
     height: rawData.height,
     base_experience: rawData.base_experience,
-    sprite: rawData.sprite,
+    sprites: rawData.sprites,
     typeNames,
   };
   return card;
-}
+};
 
-app.post("/add", async function (req, res) {
-  let params = processParams(req);
-  let typecard = params.type;
-  let name = params.name;
-  let data;
-  if (typecard != "pokemon") {
-    data = params.data;
-  } else {
-    data = await getPokemonCard(name);
-  }
-  let card = { name: name, typecard: typecard, data: data };
-  mazo[`${name}`] = card;
-  res.send("Success");
-});
 
-app.get("/get/:id", function (req, res) {
-  let params = processParams(req);
-  let name = params.id; //Utilizamos el nombre de la carta como ID
-  let card = mazo[`${name}`];
-  if (card == null) {
-    res.send("NOT FOUND");
-  } else {
-    res.send(card);
-  }
-});
 
-app.delete("/delete/:id", function (req, res) {
-  let params = processParams(req);
-  let name = params.id; //Utilizamos el nombre de la carta como ID
-  delete mazo[`${name}`];
-  res.send("Success");
-});
+  app.listen(3000);
 
-app.get("/get", function (req, res) {
-  let card = mazo;
-  if (card == null) {
-    res.send("NOT FOUND");
-  } else {
-    res.send(card);
-  }
-});
-
-app.put("/put/:id", function (req, res) {
-  let params = processParams(req);
-  let name = params.id;
-  let data = params.data; //el parseo de la data será desde front, data contiene todo
-  mazo[`${name}`].data = data;
-  console.log(mazo);
-  res.send("Success");
-});
-
-app.listen(3000);
